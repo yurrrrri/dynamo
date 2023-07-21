fddb736eeaf4c40d36cf1fb32f7e93915267c832
yurison <yu_ri_son@naver.com>
2023-07-22 00:25:37 +0900

    init

---
+++ src/test/java/com/syr/dynamo/DynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syr.dynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
