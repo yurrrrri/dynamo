750d264086c11f4e3e32b47c15359608ee619634
yurison <yu_ri_son@naver.com>
2023-07-22 19:34:59 +0900

    feat: Post 저장(등록)

---
+++ src/main/java/com/syr/dynamo/global/aws/dynamoDb/DynamoDbConfig.java
@@ -0,0 +1,38 @@
+package com.syr.dynamo.global.aws.dynamoDb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration(enforceUniqueMethods = false)
+public class DynamoDbConfig {
+
+    @Value("${aws.accessKeyId}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKey, awsSecretKey)))
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+    }
+}
--- src/main/java/com/syr/dynamo/repository/PostRepository.java
+++ src/main/java/com/syr/dynamo/repository/PostRepository.java
@@ -1,16 +1,33 @@
 package com.syr.dynamo.repository;
 
 import com.syr.dynamo.entity.Post;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.BeanTableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Repository
+@RequiredArgsConstructor
 public class PostRepository {
+
+    private final DynamoDbEnhancedClient dynamoDbEnhancedClient;
+    private static final String TABLE_NAME = "post";
+
     public Post save(Post post) {
         post.setId(UUID.randomUUID());
         post.setCreateDate(LocalDateTime.now().toString());
+
+        DynamoDbTable<Post> table = dynamoDbEnhancedClient.table(TABLE_NAME, BeanTableSchema.create(Post.class));
+        PutItemEnhancedRequest<Post> putItemEnhancedRequest = PutItemEnhancedRequest.builder(Post.class)
+                .item(post)
+                .build();
+
+        table.putItem(putItemEnhancedRequest);
 
         return post;
     }
